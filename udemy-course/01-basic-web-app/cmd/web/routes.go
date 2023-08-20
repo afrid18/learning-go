e13b3ba16c8eb2eacd483a532f2a3aa2698b07c3
afrid18 <afridhussain.d@gmail.com>
2023-08-20 22:58:08 +0530

    Day2: Routing & Middleware

--- udemy-course/01-basic-web-app/cmd/web/routes.go
+++ udemy-course/01-basic-web-app/cmd/web/routes.go
@@ -3,17 +3,26 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 
 	"github.com/afrid18/basicwebapp/pkg/config"
 	"github.com/afrid18/basicwebapp/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
+	//
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
