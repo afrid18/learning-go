bdedf543c064bafd1cfcba153e7d3d2da7083cf2
afrid18 <afridhussain.d@gmail.com>
2025-01-18 23:27:20 +0530

    pre learn go with tests

---
+++ udemy-course/02-bed-and-breakfast/pkg/handlers/handlers.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/afrid18/basicwebapp/pkg/config"
+	"github.com/afrid18/basicwebapp/pkg/models"
+	"github.com/afrid18/basicwebapp/pkg/render"
+)
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+var Repo *Repository
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	StringMap := make(map[string]string)
+	StringMap["test"] = "Hello"
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	StringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: StringMap,
+	})
+}
