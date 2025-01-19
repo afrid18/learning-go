ab8bab6e6f12260dc19a5ceda78de1a279d01824
afrid18 <afridhussain.d@gmail.com>
2025-01-20 01:35:17 +0530

    learnGoWithTests: Array and Slices - 2

--- learn-go-with-tests/Arrays and Slices/sum.go
+++ learn-go-with-tests/Arrays and Slices/sum.go
@@ -1,9 +1,35 @@
 package main
 
-func Sum(arr []int) int{
-  total := 0;
-  for _, number := range arr {
-    total += number
-  }
-  return total
+func Sum(arr []int) int {
+	total := 0
+	for _, number := range arr {
+		total += number
+	}
+	return total
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
