4f188f6eb93e42be688c3ee5720a0b225a96e22d
afrid18 <afridhussain.d@gmail.com>
2025-01-22 02:48:36 +0530

    learnGoWithTests: Structs methods & interfaces - 2

--- learn-go-with-tests/Structs, methods & interfaces/shapes_test.go
+++ learn-go-with-tests/Structs, methods & interfaces/shapes_test.go
@@ -35,19 +35,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		hasArea  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-    {shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-    {shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-    {shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
 
-		if got != tt.hasArea {
-			t.Errorf("%#v got %g want %g", tt, got, tt.hasArea)
-		}
+			got := tt.shape.Area()
+
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt, got, tt.hasArea)
+			}
+		})
 	}
 }
